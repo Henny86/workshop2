c58d7b3309bf00c03d541b5675a605f1494d3838
maurice <maurice@lolcat>
2016-07-23 12:46:10 +0200

    bestellingcontroller

---
+++ workshopdeel2/src/main/java/web/BestellingController.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import POJOs.BestelArtikel;
+import service.BestellingService;
+import POJOs.Bestelling;
+
+@Controller
+public class BestellingController {
+	
+	private BestellingService bestellingService;
+	
+	@Autowired(required=true)
+	@Qualifier(value="bestellingService")
+	public void setBestellingService(BestellingService ps){
+		this.bestellingService = ps;
+	}
+	
+	@RequestMapping(value = "/bestelling", method = RequestMethod.GET)
+	public String listBestelling(Model model, long ID) {
+		model.addAttribute("bestelartikel", new BestelArtikel());
+		model.addAttribute("listArtikel", this.bestellingService.findById(ID).getBestelArtikelSet());
+		return "bestelartikel";
+	}
+	
+	//For add and update person both
+	@RequestMapping(value= "/bestelling/add", method = RequestMethod.POST)
+	public String addArtikel(@ModelAttribute("bestelartikel") BestelArtikel p, long ID){
+		
+		if(p.getBestelling_artikel_id() == 0){
+			//new person, add it
+                        BestellingService b = new BestellingService();
+                        Bestelling bestelling = b.findById(ID);
+                        bestelling.getBestelArtikelSet().add(p);
+			this.bestellingService.update(bestelling);
+		}
+		
+		return "redirect:/listArtikel";
+		
+	}
+	
+	@RequestMapping("/remove/{id}")
+    public String removeArtikel(@PathVariable("id") long id, BestelArtikel bestelArtikel){
+                        BestellingService b = new BestellingService();
+                        Bestelling bestelling = b.findById(id);
+                        bestelling.getBestelArtikelSet().remove(bestelArtikel);
+			this.bestellingService.update(bestelling);		
+        return "redirect:/listArtikel";
+    }
+ 
+    @RequestMapping("/edit/{id}")
+    public String editArtikel(@PathVariable("id") int id, Model model, BestelArtikel bestelArtikel){
+		model.addAttribute("bestelartikel", new BestelArtikel());
+		model.addAttribute("listArtikel", this.bestellingService.findById(id).getBestelArtikelSet());
+                        BestellingService b = new BestellingService();
+                        Bestelling bestelling = b.findById(id);
+                        bestelling.getBestelArtikelSet().remove(bestelArtikel); //haal bestelArtikel met oude gegevens eerst weg
+                        bestelling.getBestelArtikelSet().add(bestelArtikel); // voeg bestelArtikel met geupdate gegevens toe.
+			this.bestellingService.update(bestelling);
+        return "artikel";
+    }
+	
+}
